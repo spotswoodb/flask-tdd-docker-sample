2d5aba084c69aba03a45cdf3bc33793cd63eea9b
Benjamin <spotswoodb@gmail.com>
2023-01-17 09:55:33 -0500

    docker synced and working

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, jsonify
 from flask_restx import Resource, Api
 
@@ -5,6 +7,10 @@ from flask_restx import Resource, Api
 app = Flask(__name__)
 
 api = Api(app)
+
+# set config
+app_settings = os.getenv('APP_SETTINGS')  # new
+app.config.from_object(app_settings)   
 
 app.config.from_object('src.config.DevelopmentConfig')  # new
 
