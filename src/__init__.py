5e937f6e38d48fc148e960b65b901d87ebb857fa
Benjamin <spotswoodb@gmail.com>
2023-01-20 13:17:26 -0500

    postgres setup with docker

--- src/__init__.py
+++ src/__init__.py
@@ -2,6 +2,7 @@ import os
 
 from flask import Flask, jsonify
 from flask_restx import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
 
 # instantiate the app
 app = Flask(__name__)
@@ -12,7 +13,21 @@ api = Api(app)
 app_settings = os.getenv('APP_SETTINGS')  # new
 app.config.from_object(app_settings)   
 
-app.config.from_object('src.config.DevelopmentConfig')  # new
+# app.config.from_object('src.config.DevelopmentConfig')  # new
+
+db = SQLAlchemy(app)
+
+# model
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    active = db.Column(db.Boolean(), default=True, nullable=False)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
 
 class Ping(Resource):
     def get(self):
@@ -20,5 +35,8 @@ class Ping(Resource):
             'status': 'success',
             'message': 'pong!'
         }
+
+# import sys
+# print(app.config, file=sys.stderr)
 
 api.add_resource(Ping, '/ping')
